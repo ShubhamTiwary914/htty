ceb8d8121172a03fef9847295d0685fb31e79866
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2026-03-31 22:46:45 +0530

    scrollable pager for response + loading indicator

--- panels/response.go
+++ panels/response.go
@@ -1,13 +1,11 @@
 package htty
 
 import (
-	"strings"
 	"fmt"
 	components "htty/panels/components"
 	types "htty/types"
 	utils "htty/utils"
 	"slices"
-	"time"
 
 	global "htty/globals"
 	"os"
@@ -18,7 +16,7 @@ import (
 )
 
 type ResponsePane struct {
-	textpane components.TextPane
+	textpane components.PagerPane
 	//objects gets filled after API call from main -> response
 	bodyRaw string
 	headersRaw map[string]string
@@ -72,16 +70,13 @@ func (res *ResponsePane) SetResponse(formattedResp string, rawBody string, heade
 	res.bodyRaw = rawBody
 	res.headersRaw = headers
 	res.status = status
-	res.textpane.Input.SetValue(formattedResp)
+	res.textpane.SetContent(formattedResp)
 }
 
-func NewResponseTextComponent() components.TextPane {
-	var responseTextComponent = components.TextPane{
-		CharLimit:   2147483647,
+func NewResponseTextComponent() components.PagerPane {
+	var responseTextComponent = components.PagerPane {
 		PanelTitle: global.Config.Panels.Main_res.Title,
 		PanelID:     global.PANEL_RES,
-		Placeholder: "response will appear here... (API call with ctrl+enter)",
-		Showline:    false,
 		Border:      types.BorderConfig{Bottom: true, Top: true, Left: true, Right: true},
 		StatusOptions: utils.GetPanel_KeyOptions(global.Config.Panels.Main_res),
 	}
@@ -94,7 +89,7 @@ func (res *ResponsePane) saveDialog() {
     if err != nil {
        utils.Errorf("%v", err) 
     }	
-	os.WriteFile(filename, []byte(res.textpane.Input.Value()), 0644)
+	os.WriteFile(filename, []byte(res.textpane.Content), 0644)
 }
 
 //saves body content to temp dir's random file and opens that file in browser/editor/etc...
@@ -102,7 +97,7 @@ func (res *ResponsePane) openView() {
 	fileType := utils.GetResponseContent_FileExtension(res.headersRaw["Content-Type"])
 	randomFileNameUUID := utils.GenerateRandomUUID(16)
 	filePath := fmt.Sprintf("%s/%s.%s", global.TEMP_DIR, randomFileNameUUID, fileType)
-	content := res.textpane.Input.Value() 
+	content := res.textpane.Content 
 	err := utils.WriteFileContents(filePath, content)
 	if err != nil {
 		panic(err)
@@ -111,7 +106,7 @@ func (res *ResponsePane) openView() {
 }
 
 func (res *ResponsePane) copyClipboard() {
-	clipboard.WriteAll(res.textpane.Input.Value())	
+	clipboard.WriteAll(res.textpane.Content)	
 }
 
 func (res *ResponsePane) verboseToggle(){
@@ -119,20 +114,4 @@ func (res *ResponsePane) verboseToggle(){
 	var newOutput string = utils.ResponseParser_main([]byte(res.bodyRaw), res.headersRaw, 
 								res.status, res.verboseAllow) 
 	res.SetResponse(newOutput, string(res.bodyRaw), res.headersRaw, res.status)
-}
-
-func (res *ResponsePane) HandleLoadTimer(){
-	loadTimer := time.NewTicker(1 * time.Second) 
-	var dots strings.Builder
-	go func () {
-		for range loadTimer.C {
-			if !res.loading {
-				loadTimer.Stop()
-				return;
-			} 
-			utils.Debugf("Timer ticked!!")
-			dots .WriteString(".")
-			res.SetResponse("Loading" + dots.String(), "", nil, 0)
-		}
-	}()
 }
